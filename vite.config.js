b21760e554c55f4e099bb61eb975a1f0b591147f
Sandun Jayalath <sandunsathyajith1@gmail.com>
2025-05-14 08:32:53 +0530

    Re-add images with proper tracking

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   build: {
     outDir: 'build',
+    assetsDir: 'assets',
   },
   plugins: [react()],
   base: '/NobleDrive-Dealership/',
