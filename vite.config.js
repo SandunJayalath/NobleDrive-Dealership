6420d9da0e7b6bf1889363781138fc329b2c658d
Sandun Jayalath <sandunsathyajith1@gmail.com>
2025-05-14 11:51:04 +0530

    Remove build folder from Git

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Change 'build' to 'dist'
+    assetsDir: 'assets',
+  },
   base: '/NobleDrive-Dealership/', // ✅ Important!
   plugins: [react()],
 })
